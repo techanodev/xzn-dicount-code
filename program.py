59b1285b7ea51a3f96a51a04c29b911e45e5c2c7
arminhero1 <ArminHosseyninezhad@gmail.com>
2022-05-07 13:03:26 +0430

    Make better view.
    Co-authored-by: Abolfazl Alizadeh Sh <mrabolfazlalz@gmail.com>

--- program.py
+++ program.py
@@ -1,27 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
+import inquirer
+from progress.spinner import Spinner
 
 page = requests.get('https://xzn.ir/discount-code')
 
-soup = BeautifulSoup(page.text, 'html.parser')
-for slice in soup.find_all('div', {'class': 'wof-slice'}):
-    print(slice['data-slice'], '.', slice.get_text())
+slices = BeautifulSoup(page.text, 'html.parser').find_all('div', {'class': 'wof-slice'})
+slices_text = [slice.get_text() for slice in slices]
 
-slice_id = int(input('Enter slice id: '))
+questions = [
+  inquirer.List('segment',
+                message="which one ?",
+                choices=slices_text,
+            ),
+]
+answer = inquirer.prompt(questions)['segment']
+
+slice_id = int(slices[slices_text.index(answer)]['data-slice'])
 
 headers = {
     "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
 }
 body = "action=wof-email-optin&id=3363&seq=TGVLSkpnaE9ySXdxdm84aDdUM3ZHTlJSWnQ0a0Q5YXYvVnQva0d3Y3JWOD0%3D&pseq=TGVLSkpnaE9ySXdxdm84aDdUM3ZHTlJSWnQ0a0Q5YXYvVnQva0d3Y3JWOD0%3D&nonce=3953b4c0c0"
 
-i = 0
+
+sp = Spinner('Please wait...')
 while True:
-    i += 1
+    sp.next()
     result = requests.post(
         'https://xzn.ir/wp-admin/admin-ajax.php', data=body, headers=headers)
 
     data = result.json()['data']
-    print(i, data['segment'])
+
     if data['segment'] == slice_id:
-        print(data['value'])
+        print("\n\rYour code: ", data['value'])
+        sp.finish()
         break
